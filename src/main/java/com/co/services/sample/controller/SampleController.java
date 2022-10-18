575114eb359c7c62362f4948cd2cf0d90f5b7e64
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-10-18 16:48:05 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -2,29 +2,55 @@ package com.co.services.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.Console;
+
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.DeletedSecret;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 @RestController
 public class SampleController {
-    @Autowired
-    private Environment environment;
-
-    private final Logger log = LoggerFactory.getLogger(SampleController.class);
-
+    
     @GetMapping("/")
-    public String sampleApi() {
-        String serverName = environment.getProperty("environment.servername");
-        String port = environment.getProperty("environment.dbport");
-        String databaseName = environment.getProperty("environment.dbname");
-        String userName = environment.getProperty("environment.username");
-        String password = environment.getProperty("environment.password");
-        String envSample = environment.getProperty("environment.username");
-        log.info("********************************* logging env var from Config map/yaml");
-        log.info(envSample);
+    public String sampleApi() throws InterruptedException, IllegalArgumentException {
+        String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        //secret start
+        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+
+        System.out.printf("key vault name = %s and key vault URI = %s \n", keyVaultName, keyVaultUri);
+
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(keyVaultUri)
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+
+        String secretName = "mySecret";
+
+        System.out.println("Please provide the value of your secret > ");
+        
+        String secretValue = "SampleSecretValue!";
+
+        System.out.print("Creating a secret in " + keyVaultName + " called '" + secretName + "' with value '" + secretValue + "` ... ");
+
+        secretClient.setSecret(new KeyVaultSecret(secretName, secretValue));
+
+        System.out.println("Retrieving your secret from " + keyVaultName + ".");
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+
+        System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
+        System.out.print("Deleting your secret from " + keyVaultName + " ... ");
+
+        SyncPoller<DeletedSecret, Void> deletionPoller = secretClient.beginDeleteSecret(secretName);
+        deletionPoller.waitForCompletion();
+
+        System.out.println("done.");
+        //secret end
         return "Hello Sample Microservice 2";
     }
     
