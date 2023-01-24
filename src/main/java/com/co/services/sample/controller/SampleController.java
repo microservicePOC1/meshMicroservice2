dc82667d9afa34f4bba2c6d4612910e669da1a5e
charley43613 <41710701+charley43613@users.noreply.github.com>
2023-01-24 13:25:00 -0500

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -1,23 +1,26 @@
-package com.service.keyvault.controller;
+package com.co.services.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.Console;
 
 import org.springframework.beans.factory.annotation.Value;
 
 
 @RestController
 public class SampleController {
-
-    
     @Value("${connectionString}")
     private String connectionString;
     
+    @GetMapping("/")
+    public String sampleApi() throws InterruptedException, IllegalArgumentException {
+        
+        return "Hello Sample Microservice 2";
+    }
     
     @GetMapping("get")
     public String get() {
         return connectionString;
     }
-
     
 }
