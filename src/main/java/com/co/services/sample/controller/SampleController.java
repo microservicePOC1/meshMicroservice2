7913f38fa84f5804877ae108dcceff90789579bf
charley43613 <41710701+charley43613@users.noreply.github.com>
2023-01-30 16:23:17 -0500

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.service.keyvault.controller;
+package com.services.sample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
