dc422e21d76a6ff18f68f1bd7ec121d4282d5901
charley43613 <41710701+charley43613@users.noreply.github.com>
2023-01-30 14:19:32 -0500

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.beans.factory.annotation.Value;
 
