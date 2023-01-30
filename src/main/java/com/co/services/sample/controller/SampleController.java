bda4159a6c876209b07abfbf64008c591e069abe
charley43613 <41710701+charley43613@users.noreply.github.com>
2023-01-30 16:30:42 -0500

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.services.sample.controller;
+package com.co.services.sample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
